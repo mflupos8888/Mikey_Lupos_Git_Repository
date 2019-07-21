c34d6c463ed9de348fed416fcd34f8357f1b01c2
mflupos8888 <mflupos@outlook.com>
2019-07-21 15:30:18 +0800

    Commit changes 2019-07-21 15:30hr
    Commit changes 2019-07-21 15:30hr

--- STS4_Workbench/spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
+++ STS4_Workbench/spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -2,7 +2,7 @@
 <html>
 <head><title>Hello World - Input Form</title></head>
 <body>
-	<form action="processForm" method="get">
+	<form action="hello/processFormVersionThree" method="get">
 		<input type="text" name="studentName"
 			placeholder="What's your name?">
 		<input type="submit">
--- STS4_Workbench/spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ STS4_Workbench/spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// define a controller method to show the form
@@ -17,4 +22,43 @@ public class HelloWorldController {
 	public String processForm() {
 		return "helloworld";
 	}
+	
+	// define a new controller method to read data and 
+	// add data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert to upper case
+		theName = theName.toUpperCase();
+		
+		// create message
+		String result = "Hey! " + theName + "! You're so gorgeous!";
+		
+		// add message to the model
+		model.addAttribute("message",result);
+		
+		// return via helloworld.jsp
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, Model model) {
+		
+		// convert to upper case
+		theName = theName.toUpperCase();
+		
+		// create message
+		String result = "Hey! My friend " + theName + "! You're so gorgeous!";
+		
+		// add message to the model
+		model.addAttribute("message",result);
+		
+		// return via helloworld.jsp
+		return "helloworld";
+	}
+
 }
