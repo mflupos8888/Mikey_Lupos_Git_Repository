fa1666138e75074fabf069b073547b43bfac8704
mflupos8888 <mflupos@outlook.com>
2019-07-24 22:51:12 +0800

    Commit changes 2019-07-24 22:50hr
    Commit changes 2019-07-24 22:50hr

---
+++ STS4_Workbench/hibernate-tutorial/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,45 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// create a Session Factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		// create a Session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create a student object
+			System.out.println("Create new student object...");
+			Student tempStudent = new Student("Mikey", "Lupos", "mikeylupos@email.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student object into MySQL student table...");
+			session.save(tempStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Update committed...");
+			
+		}
+		finally {
+			factory.close();
+		}
+
+	}
+
+}
---
+++ STS4_Workbench/hibernate-tutorial/src/com/luv2code/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,64 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.entity.Student;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// create a Session Factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		// create a Session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create a student object
+			System.out.println("Create new student object...");
+			Student tempStudent = new Student("Mikey2", "Lupos2", "mikey2lupos2@email.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student object into MySQL student table...");
+			System.out.println("Saving student : " + tempStudent);
+			session.save(tempStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Update committed...");
+			
+			//find out the student's primary key
+			System.out.println("Saved student's key : " + tempStudent.getId());
+			
+			// get a new session
+			session = factory.getCurrentSession();
+			
+			// start a new transaction
+			session.beginTransaction();
+			
+			//retrieve the Saved student's record based on the primary key
+			System.out.println("Key of student to get : " + tempStudent.getId());
+			Student getStudent = session.get(Student.class, tempStudent.getId());
+			System.out.println("Retrieved student details : ");
+			System.out.println("               Id         : " + getStudent.getId());
+			System.out.println("               First Name : " + getStudent.getFirstName());
+			System.out.println("               Last  Name : " + getStudent.getLastName());
+			System.out.println("               Email      : " + getStudent.getEmail());
+			
+		}
+		finally {
+			factory.close();
+		}
+
+	}
+
+}
